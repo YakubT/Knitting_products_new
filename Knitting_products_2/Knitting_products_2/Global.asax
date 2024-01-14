8d808248404fba43ea550c3682505f58aea93dad
= <goldyakub@gmail.com>
2024-01-14 19:05:58 +0200

    implemented frontend for home page

---
+++ Knitting_products_2/Knitting_products_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Knitting_products_2.MvcApplication" Language="C#" %>
---
+++ Knitting_products_2/Knitting_products_2/Scripts/lay.js
@@ -0,0 +1,10 @@
+﻿$('#hamburger').click( () => {
+	let disp = $('.navbar .container-fluid').css('display');
+	if (disp === 'none') {
+		$('.navbar .container-fluid').css('display', 'block');
+		$('.body-content').css('margin-top', '200px');
+	} else {
+		$('.navbar .container-fluid').css('display', 'none');
+		$('.body-content').css('margin-top', '100px');
+	}
+})
